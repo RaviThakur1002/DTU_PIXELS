1fb543beef23d14d2cd62dc3a943cc10104a6705
Pratyush Sudarshan <pratyushsudarshan1607@gmail.com>
2024-07-02 18:45:52 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import authService from './firbase/auth/auth.js'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='text-white bg-black border-sky-50 underline'>DTU PIXELS</h1> 
-      <button onClick={authService.googleSignIn}>sign in</button>
+      <Navbar />
     </>
   )
 }
