e32e740b8b46c9e8a85d71d26615bee09ef0d39b
Ravi Ranjan <ranjanravi5667@gmail.com>
2024-07-31 18:49:39 +0530

    updated voting system

--- src/components/contests/voting/VoteCard.jsx
+++ src/components/contests/voting/VoteCard.jsx
@@ -27,22 +27,24 @@ const VoteCard = ({ entry, isLiked, onLike, onClick }) => {
           onLike();
         }}
         whileTap={{ scale: 0.9 }}
+        transition={{ duration: 0.1 }}
       >
         <motion.svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6"
-          fill={isLiked ? 'white' : 'none'}
-          viewBox="0 0 24 24"
-          stroke={isLiked ? 'white' : 'currentColor'}
+          viewBox="0 0 20 20"
           initial={{ scale: 1 }}
-          animate={{ scale: isLiked ? [1, 1.2, 1] : 1 }}
-          transition={{ duration: 0.3 }}
+          animate={{ 
+            scale: isLiked ? [1, 1.3, 1] : 1,
+            fill: isLiked ? '#ffffff' : 'none',
+            stroke: isLiked ? '#ffffff' : '#374151'
+          }}
+          transition={{ duration: 0.2 }}
         >
           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            fillRule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+            clipRule="evenodd"
           />
         </motion.svg>
       </motion.button>
