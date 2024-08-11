0986ae3e9aa9d3c3a492ae8e95eb5ff5633ad3e9
04samarthg <04samarthgupta@gmail.com>
2024-08-11 19:17:50 +0530

    updation as needed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,6 +16,7 @@ const Navbar = () => {
   const [isAdmin, setIsAdmin] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [isPromotionPopupOpen, setIsPromotionPopupOpen] = useState(false);
+  const [isFloating, setIsFloating] = useState(false);
   const dropdownRef = useRef(null);
   const sidebarRef = useRef(null);
 
@@ -65,6 +66,16 @@ const Navbar = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      setIsFloating(scrollPosition > 50); // Adjust this value as needed
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   const menuItems = [
     { name: "Home", href: "/" },
     { name: "Gallery", href: "gallery" },
@@ -77,8 +88,14 @@ const Navbar = () => {
   }
 
   return (
-    <div className="sticky z-50 top-0 w-full bg-gray-900 shadow-md">
-      <div className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 sm:px-6 lg:px-8">
+    <div className={ `sticky z-50 top-0 w-full p-1 ${isFloating ? "bg-transparent" : "bg-gray-900"}` }>
+      <div
+        className={`mx-auto flex justify-between max-w-7xl transition-all duration-300 ${
+          isFloating
+            ? "bg-white text-gray-900 rounded-full shadow-lg mt-2 px-8 py-1 border-2 border-orange-400"
+            : "bg-gray-900 text-white px-2"
+        }`}
+      >
         {/* Logo and brand name */}
         <div className="inline-flex items-center space-x-2">
           <span>
@@ -91,17 +108,21 @@ const Navbar = () => {
             >
               <path
                 d="M23.2732 0.2528C20.8078 1.18964 2.12023 12.2346 1.08477 13.3686C0 14.552 0 14.7493 0 27.7665C0 39.6496 0.0986153 41.1289 0.83823 42.0164C2.12023 43.5449 23.2239 55.4774 24.6538 55.5267C25.9358 55.576 46.1027 44.3832 48.2229 42.4602C49.3077 41.474 49.3077 41.3261 49.3077 27.8158C49.3077 14.3055 49.3077 14.1576 48.2229 13.1714C46.6451 11.7415 27.1192 0.450027 25.64 0.104874C24.9497 -0.0923538 23.9142 0.00625992 23.2732 0.2528ZM20.2161 21.8989C20.2161 22.4906 18.9835 23.8219 17.0111 25.3997C15.2361 26.7803 13.8061 27.9637 13.8061 28.0623C13.8061 28.1116 15.2361 29.0978 16.9618 30.2319C18.6876 31.3659 20.2655 32.6479 20.4134 33.0917C20.8078 34.0286 19.871 35.2119 18.8355 35.2119C17.8001 35.2119 9.0233 29.3936 8.67815 28.5061C8.333 27.6186 9.36846 26.5338 14.3485 22.885C17.6521 20.4196 18.4904 20.0252 19.2793 20.4196C19.7724 20.7155 20.2161 21.3565 20.2161 21.8989ZM25.6893 27.6679C23.4211 34.9161 23.0267 35.7543 22.1391 34.8668C21.7447 34.4723 22.1391 32.6479 23.6677 27.9637C26.2317 20.321 26.5275 19.6307 27.2671 20.3703C27.6123 20.7155 27.1685 22.7864 25.6893 27.6679ZM36.0932 23.2302C40.6788 26.2379 41.3198 27.0269 40.3337 28.1609C39.1503 29.5909 31.6555 35.2119 30.9159 35.2119C29.9298 35.2119 28.9436 33.8806 29.2394 33.0424C29.3874 32.6479 30.9652 31.218 32.7403 29.8867L35.9946 27.4706L32.5431 25.1532C30.6201 23.9205 29.0915 22.7371 29.0915 22.5892C29.0915 21.7509 30.2256 20.4196 30.9159 20.4196C31.3597 20.4196 33.6771 21.7016 36.0932 23.2302Z"
-                fill="white"
+                fill="gray"
               />
             </svg>
           </span>
           <NavLink to={"/"}>
-            <span className="font-bold text-white">DTU PIXELS</span>
+            <span
+              className={`font-bold ${isFloating ? "text-gray-900" : "text-white"}`}
+            >
+              DTU PIXELS
+            </span>
           </NavLink>
         </div>
 
         {/* Desktop menu */}
-        <div className="hidden grow items-start lg:flex">
+        <div className="hidden grow items-start lg:flex pt-1">
           <ul className="ml-12 inline-flex space-x-8">
             {menuItems.map((item) => (
               <li key={item.name} className="relative group">
@@ -109,7 +130,11 @@ const Navbar = () => {
                   to={item.href}
                   className={({ isActive }) =>
                     `inline-flex items-center text-md font-semibold ${
-                      isActive ? "text-orange-500" : "text-white"
+                      isActive
+                        ? "text-orange-500"
+                        : isFloating
+                          ? "text-gray-900"
+                          : "text-white"
                     } hover:text-orange-300 py-2`
                   }
                 >
@@ -129,30 +154,36 @@ const Navbar = () => {
             <div className="relative" ref={dropdownRef}>
               <button
                 onClick={() => setDropdownOpen(!dropdownOpen)}
-                className="flex items-center focus:outline-none gap-2 p-1"
+                className={`flex items-center focus:outline-none gap-2 p-1 ${
+                  isFloating ? "text-gray-900" : "text-white"
+                }`}
               >
                 <img
                   className="rounded-full w-10 h-10"
                   src={user.photoURL}
                   alt="User profile"
                 />
-                <ChevronDown className="h-4 w-4 ml-1 text-white" />
+                <ChevronDown className={ `h-4 w-4 ml-1 ${isFloating ? "text-black": "text-white"}`  }/>
               </button>
               {dropdownOpen && (
-                <div className="absolute z-50 right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-xl py-1"> {/* Changed background */}
-                  <div className="px-4 py-3 border-gray-700 flex flex-col items-center justify-center"> {/* Changed border color */}
-                    <h3 className="text-md font-bold text-white mb-1"> {/* Changed text color */}
+                <div
+                  className={`absolute z-50 right-0 mt-2 w-48 rounded-md shadow-xl py-1 ${
+                    isFloating ? "bg-gray-700" : "bg-gray-700"
+                  }`}
+                >
+                  <div className="px-4 py-3 border-gray-700 flex flex-col items-center justify-center">
+                    <h3 className="text-md font-bold text-white mb-1">
                       {user.displayName}
                     </h3>
                     {isAdmin && (
-                      <span className="text-xs font-semibold text-orange-500"> {/* Changed color */}
+                      <span className="text-xs font-semibold text-orange-500">
                         Admin
                       </span>
                     )}
                   </div>
-                  <CopyUID className="border-t border-gray-700 my-1" /> {/* Changed border color */}
-                  <div className="border-t border-gray-700 my-1"></div> 
-
+                  <CopyUID className="border-t border-gray-700 my-1" />{" "}
+                  {/* Changed border color */}
+                  <div className="border-t border-gray-700 my-1"></div>
                   {isAdmin && (
                     <>
                       <CreateContest />
@@ -168,7 +199,6 @@ const Navbar = () => {
                       </button>
                     </>
                   )}
-
                   <button
                     onClick={authService.signOutUser}
                     className="w-full mt-2 px-1 py-2 text-sm font-medium text-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-colors duration-300"
@@ -190,7 +220,11 @@ const Navbar = () => {
             </div>
           ) : (
             <button
-              className="flex items-center justify-center px-3 py-2 bg-orange-500 text-white border border-orange-600 rounded-md shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 transition-colors duration-300"
+              className={`flex items-center justify-center px-3 py-2 ${
+                isFloating
+                  ? "bg-orange-500 text-white"
+                  : "bg-white text-gray-900"
+              } border border-orange-600 rounded-md shadow-sm hover:bg-orange-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 transition-colors duration-300`}
               onClick={authService.googleSignIn}
             >
               <svg
@@ -221,7 +255,12 @@ const Navbar = () => {
         </div>
 
         <div className="lg:hidden">
-          <Menu onClick={toggleSidebar} className="h-6 w-6 cursor-pointer text-white" />
+          <Menu
+            onClick={toggleSidebar}
+            className={`h-6 w-6 cursor-pointer ${
+              isFloating ? "text-gray-900" : "text-white"
+            }`}
+          />
         </div>
 
         {/* Mobile Sidebar */}
