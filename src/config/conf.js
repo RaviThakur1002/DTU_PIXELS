d3e6d850e1d2679d4f4fb9167100b3096b740a4b
Ravi Ranjan <ranjanravi5667@gmail.com>
2024-08-17 23:00:12 +0530

    .env file is created

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,19 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAlc_1loWvnnQVipYb2ClylUDF4db7pVuM",
-  authDomain: "dtu-pixels.firebaseapp.com",
-  projectId: "dtu-pixels",
-  storageBucket: "dtu-pixels.appspot.com",
-  messagingSenderId: "733502370431",
-  appId: "1:733502370431:web:94b756bd5816906578c54a",
-  measurementId: "G-EMEMPYH7DV",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -21,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 export default app;
+
