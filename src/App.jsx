0be5a10c4a10defcd91477314142e3c1b23f66fd
04samarthg <04samarthgupta@gmail.com>
2024-12-26 18:01:45 +0530

    removed extra routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Home from "./components/Home/Home.jsx";
 import Gallery from "./components/gallery/Gallery.jsx";
 import Profile from "./components/Profile/Profile.jsx";
 import Temp from "./components/contests/temp/Temp.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import CreateContest from "./components/Admin/CreateContest.jsx";
-import ContestVoting from "./components/contests/voting/ContestVoting.jsx";
 import PerContestPage from "./components/contests/perContestPage/PerContestPage.jsx";
 import { GalleryProvider } from "./components/contexts/GalleryContext.jsx";
 import { ContestProvider } from "./components/contexts/ContestContext.jsx";
@@ -26,8 +23,6 @@ const App = () => {
             <Route path="*" element={<HomeScreen /> } />
             <Route path="/contest" element={<Temp />} />
             <Route path="/submissions" element={<Profile />} />
-            <Route path="/contest/createcontest" element={<CreateContest />} />
-            <Route path="/contest/voting" element={<ContestVoting />} />
             <Route path="/contest/:contestId" element={<PerContestPage />} />
             <Route path="/winners" element={<HallOfFame />} />
           </Routes>
